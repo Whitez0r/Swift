0006ba208012aac57192d1fece86728b52673212
ilya Mungalov <63450920+Whitez0r@users.noreply.github.com>
2022-03-09 16:38:07 +0300

    fixed the comments

--- hw_3/hw_3/ViewController.swift
+++ hw_3/hw_3/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: UIViewController {
              
         /* Универсальные функции сложения, вычитания, умножения и деления (перегрузка функций, одинаковые названия для функций, разные типы параметров)*/
         
-        summa(3, 4)
-        summa(3.4, 5)
-        summa(4, 3.5)
-        summa(23.43, 23.64)
+        sum(3, 4)
+        sum(3.4, 5)
+        sum(4, 3.5)
+        sum(23.43, 23.64)
         
         subtraction(5, 4)
         subtraction(6.4, 5)
@@ -29,109 +29,109 @@ class ViewController: UIViewController {
         multiply(4, 3.5)
         multiply(23.43, 23.64)
         
-        devision(5, 4)
-        devision(6.4, 5)
-        devision(6, 3.5)
-        devision(245.43, 23.64)
+       division(5, 4)
+       division(6.4, 5)
+       division(6, 3.5)
+       division(245.43, 23.64)
         
         // Вычислить сумму цифр четырехзначного числа
-        sumFour(number: 9999)
+        sumFourDigitNumberDigits(number: 9999)
         
         // Функция сравнения строк
-        stringComparison(firstString: "absfda", secondString: "absfda")
+        compareStrings(firstString: "absfda", secondString: "absfda")
         
         // Циклический вызов функций - поломать приложение
         //errorRecursion()
         
         // Функция возведения в степень с дефолтным параметром
-        numberPow(5)
+        powNumber(5)
         
         // Функция вычисления факториала числа (рекурсия)
         factorial(10)
         
     }
 
-    func summa(_ firstnumber: Int,_  secondNumber: Int) {
-        let numberSum = firstnumber + secondNumber
-        print("\(firstnumber) + \(secondNumber) = \(numberSum) ")
+    func sum(_ firstNumber: Int,_  secondNumber: Int) {
+        let numberSum = firstNumber + secondNumber
+        print("\(firstNumber) + \(secondNumber) = \(numberSum) ")
     }
     
-    func summa(_ firstnumber: Int,_  secondNumber: Double) {
-        let numberSum = Double(firstnumber) + secondNumber
-        print("\(firstnumber) + \(secondNumber) = \(numberSum) ")
+    func sum(_ firstNumber: Int,_  secondNumber: Double) {
+        let numberSum = Double(firstNumber) + secondNumber
+        print("\(firstNumber) + \(secondNumber) = \(numberSum) ")
     }
     
-    func summa(_ firstnumber: Double,_  secondNumber: Int) {
-        let numberSum = firstnumber + Double(secondNumber)
-        print("\(firstnumber) + \(secondNumber) = \(numberSum) ")
+    func sum(_ firstNumber: Double,_  secondNumber: Int) {
+        let numberSum = firstNumber + Double(secondNumber)
+        print("\(firstNumber) + \(secondNumber) = \(numberSum) ")
     }
     
-    func summa(_ firstnumber: Double,_  secondNumber: Double) {
-        let numberSum = firstnumber + secondNumber
-        print("\(firstnumber) + \(secondNumber) = \(numberSum) ")
+    func sum(_ firstNumber: Double,_  secondNumber: Double) {
+        let numberSum = firstNumber + secondNumber
+        print("\(firstNumber) + \(secondNumber) = \(numberSum) ")
     }
     
-    func subtraction(_ firstnumber: Int,_  secondNumber: Int) {
-        let numberSum = firstnumber - secondNumber
-        print("\(firstnumber) - \(secondNumber) = \(numberSum) ")
+    func subtraction(_ firstNumber: Int,_  secondNumber: Int) {
+        let numberSum = firstNumber - secondNumber
+        print("\(firstNumber) - \(secondNumber) = \(numberSum) ")
     }
     
-    func subtraction(_ firstnumber: Int,_  secondNumber: Double) {
-        let numberSum = Double(firstnumber) - secondNumber
-        print("\(firstnumber) - \(secondNumber) = \(numberSum) ")
+    func subtraction(_ firstNumber: Int,_  secondNumber: Double) {
+        let numberSum = Double(firstNumber) - secondNumber
+        print("\(firstNumber) - \(secondNumber) = \(numberSum) ")
     }
     
-    func subtraction(_ firstnumber: Double,_  secondNumber: Int) {
-        let numberSum = firstnumber - Double(secondNumber)
-        print("\(firstnumber) - \(secondNumber) = \(numberSum) ")
+    func subtraction(_ firstNumber: Double,_  secondNumber: Int) {
+        let numberSum = firstNumber - Double(secondNumber)
+        print("\(firstNumber) - \(secondNumber) = \(numberSum) ")
     }
     
-    func subtraction(_ firstnumber: Double,_  secondNumber: Double) {
-        let numberSum = firstnumber - secondNumber
-        print("\(firstnumber) - \(secondNumber) = \(numberSum) ")
+    func subtraction(_ firstNumber: Double,_  secondNumber: Double) {
+        let numberSum = firstNumber - secondNumber
+        print("\(firstNumber) - \(secondNumber) = \(numberSum) ")
     }
     
-    func multiply(_ firstnumber: Int,_  secondNumber: Int) {
-        let numberSum = firstnumber * secondNumber
-        print("\(firstnumber) * \(secondNumber) = \(numberSum) ")
+    func multiply(_ firstNumber: Int,_  secondNumber: Int) {
+        let numberSum = firstNumber * secondNumber
+        print("\(firstNumber) * \(secondNumber) = \(numberSum) ")
     }
     
-    func multiply(_ firstnumber: Int,_  secondNumber: Double) {
-        let numberSum = Double(firstnumber) * secondNumber
-        print("\(firstnumber) * \(secondNumber) = \(numberSum) ")
+    func multiply(_ firstNumber: Int,_  secondNumber: Double) {
+        let numberSum = Double(firstNumber) * secondNumber
+        print("\(firstNumber) * \(secondNumber) = \(numberSum) ")
     }
     
-    func multiply(_ firstnumber: Double,_  secondNumber: Int) {
-        let numberSum = firstnumber * Double(secondNumber)
-        print("\(firstnumber) * \(secondNumber) = \(numberSum) ")
+    func multiply(_ firstNumber: Double,_  secondNumber: Int) {
+        let numberSum = firstNumber * Double(secondNumber)
+        print("\(firstNumber) * \(secondNumber) = \(numberSum) ")
     }
     
-    func multiply(_ firstnumber: Double,_  secondNumber: Double) {
-        let numberSum = firstnumber * secondNumber
-        print("\(firstnumber) * \(secondNumber) = \(numberSum) ")
+    func multiply(_ firstNumber: Double,_  secondNumber: Double) {
+        let numberSum = firstNumber * secondNumber
+        print("\(firstNumber) * \(secondNumber) = \(numberSum) ")
     }
     
-    func devision(_ firstnumber: Int,_  secondNumber: Int) {
-        let numberSum = Double(firstnumber) / Double(secondNumber)
-        print("\(firstnumber) / \(secondNumber) = \(numberSum) ")
+    func division(_ firstNumber: Int,_  secondNumber: Int) {
+        let numberSum = Double(firstNumber) / Double(secondNumber)
+        print("\(firstNumber) / \(secondNumber) = \(numberSum) ")
     }
     
-    func devision(_ firstnumber: Int,_  secondNumber: Double) {
-        let numberSum = Double(firstnumber) / secondNumber
-        print("\(firstnumber) / \(secondNumber) = \(numberSum) ")
+    func division(_ firstNumber: Int,_  secondNumber: Double) {
+        let numberSum = Double(firstNumber) / secondNumber
+        print("\(firstNumber) / \(secondNumber) = \(numberSum) ")
     }
     
-    func devision(_ firstnumber: Double,_  secondNumber: Int) {
-        let numberSum = firstnumber / Double(secondNumber)
-        print("\(firstnumber) / \(secondNumber) = \(numberSum) ")
+    func division(_ firstNumber: Double,_  secondNumber: Int) {
+        let numberSum = firstNumber / Double(secondNumber)
+        print("\(firstNumber) / \(secondNumber) = \(numberSum) ")
     }
     
-    func devision(_ firstnumber: Double,_  secondNumber: Double) {
-        let numberSum = firstnumber / secondNumber
-        print("\(firstnumber) / \(secondNumber) = \(numberSum) ")
+    func division(_ firstNumber: Double,_  secondNumber: Double) {
+        let numberSum = firstNumber / secondNumber
+        print("\(firstNumber) / \(secondNumber) = \(numberSum) ")
     }
 
-    func stringComparison(firstString: String, secondString: String) {
+    func compareStrings(firstString: String, secondString: String) {
         if firstString > secondString {
             print("\(firstString) > \(secondString)")
         }
@@ -143,13 +143,13 @@ class ViewController: UIViewController {
         }
     }
     
-    func sumFour(number: Int) {
+    func sumFourDigitNumberDigits(number: Int) {
         let firstNumber = number / 1000
         let secondNumber = (number / 100) % 10
         let thirdNumber = (number / 10) % 10
         let fourthNumber = number % 10
-        let sumFour = firstNumber + secondNumber + thirdNumber + fourthNumber
-        print("Сумма \(number) = \(sumFour)")
+        let digitsSum = firstNumber + secondNumber + thirdNumber + fourthNumber
+        print("Сумма \(number) = \(digitsSum)")
         
     }
     
@@ -158,19 +158,19 @@ class ViewController: UIViewController {
         errorRecursion()
     }
     
-    func numberPow(_ firstNumber: Int, _ secondNumber: Int = 2) {
+    func powNumber(_ firstNumber: Int, _ secondNumber: Int = 2) {
         let powNumber = pow(Double(firstNumber), Double(secondNumber))
         print("\(firstNumber) в степени \(secondNumber) = \(powNumber)")
     }
     
     func factorial(_ numberFactorial: Int) {
         var resultFactorial = 1
-        var tamporyElement = numberFactorial
-        while tamporyElement >= 1 {
-            resultFactorial = tamporyElement * resultFactorial
-            tamporyElement = tamporyElement - 1
+        var temporaryElement = numberFactorial
+        while temporaryElement >= 1 {
+            resultFactorial = temporaryElement * resultFactorial
+            temporaryElement = temporaryElement - 1
         }
-        print("Фасториал \(numberFactorial) = \(resultFactorial)")
+        print("Факториал \(numberFactorial) = \(resultFactorial)")
     }
     
 }
