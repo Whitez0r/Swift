cbb4287481f00e0c74249a7108c162051848373b
ilya Mungalov <63450920+Whitez0r@users.noreply.github.com>
2022-03-17 10:59:11 +0300

    fixed the comments

--- hw5/hw5/ViewController.swift
+++ hw5/hw5/ViewController.swift
@@ -12,50 +12,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Создать словарь, где ключ - название города/страны, значение - интересный факт и континент, где распорожен город/страна (подумайте, какой тип данных лучше использовать для значения).
-        var cityDictionary: [String: [String]]
+        var cityDictionary: [String: (interestingFact: String, continent: String)]
         cityDictionary = [
-            "Moscow": ["Сapital of Russia", "Eurasia"],
-            "Minsk": ["Сapital of Belarus", "Eurasia"],
-            "NY": ["Finance center", "North America"],
-            "Beijing": ["Сapital of China", "Eurasia"]
+            "Moscow": ("Сapital of Russia", "Eurasia"),
+            "Minsk": ("Сapital of Belarus", "Eurasia"),
+            "NY": ("Finance center", "North America"),
+            "Beijing": ("Сapital of China", "Eurasia")
         ]
         
         //1) Добавить новый элемент в словарь, поменять два существующих элемента в словаре, удалить один элемент из словаря.
-        cityDictionary["Capetown"] = ["Сapital of South Africa", "Africa"]
+        cityDictionary["Capetown"] = ("Сapital of South Africa", "Africa")
         print(cityDictionary)
         
-        cityDictionary["Moscow"] = ["Financial center of Russia", "Eurasia"]
-        cityDictionary["NY"] = ["The Big Apple", "North America"]
+        cityDictionary["Moscow"] = ("Financial center of Russia", "Eurasia")
+        cityDictionary["NY"] = ("The Big Apple", "North America")
         print(cityDictionary)
         
-        cityDictionary["NY"] = nil
+        cityDictionary["Minsk"] = nil
         print(cityDictionary)
         
         //2) Создать отдельный словарь с названием городов/стран с длиной более 6 букв.
-        let countryDictionary = [
-            "Georgia",
-            "Azerbaijan",
-            "United Kingdom"
-        ]
+        var sortedCityDictionary: [String: (interestingFact: String, continent: String)] = [:]
         
-        print(countryDictionary)
+        for (key, value) in cityDictionary {
+            if key.count >= 6 {
+                sortedCityDictionary[key] = value
+            }
+        }
+        
+        print(sortedCityDictionary)
         
         //3) Создать массив интересных фактов в формате (“название города/страны: факт”), отсортировав их по длине строки по убыванию.
-        let interestingFactsArray = ["Capetown: Сapital of South Africa", "Moscow: Financial center of Russia", "Minsk: Сapital of Belarus", "Beijing: China"]
+        var interestingFactsArray = [String]()
+        for (key, value) in cityDictionary {
+            interestingFactsArray.append("\(key): \(value.interestingFact)")
+        }
         print(interestingFactsArray.sorted(by: { $0.count < $1.count } ))
-        //Beijing: Сapital of China
-        //Capetown: Сapital of South Africa
-        //Minsk: Сapital of Belarus
-        //Moscow: Financial center of Russia
         
         //4) * Отсортировать страны/города по континенту. Отсортированные данные должны лежать в одном объекте (сами придумайте, в каком). В итоге, у вас должна быть возможность получить все страны/города переданному континенту.
-        var continent: [String: String]
-         continent = [
-             "Africa": "Egypt, Nigeria",
-             "Asia": "Azerbaijan, India",
-             "Europe": "Austria, Ireland",
-         ]
-        print(continent["Asia"])
+        var continent: [String: String] = [:]
+
+        for (key, value) in cityDictionary {
+            continent[value.continent] = "\(key)"
+        }
+        // Не понял как получить все страны 🥺🥺
+        print(continent)
         
         //Дан массив проверить, есть ли в массиве дубликаты.
         let myArray = [1, 2, 3, 4, 5, 6, 6]
