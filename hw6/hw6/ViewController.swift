fa4d76aa9410f4158524dc0a903a8b0ece236f22
ilya Mungalov <63450920+Whitez0r@users.noreply.github.com>
2022-03-24 17:06:25 +0300

    fixed

--- hw6/hw6/ViewController.swift
+++ hw6/hw6/ViewController.swift
@@ -15,27 +15,27 @@ class ViewController: UIViewController {
         
         let intArray = [2, 3, 4, 5, 6, 7, 8, 9, 10]
         var evenArray = intArray.filter({ $0 % 2 == 0 })
-        var unevenArray = intArray.filter({ $0 % 2 != 0 })
+        var oddArray = intArray.filter({ $0 % 2 != 0 })
         
         print(evenArray)
-        print(unevenArray)
+        print(oddArray)
         
         // second option
 
         evenArray = []
-        unevenArray = []
+        oddArray = []
 
         for element in intArray {
             if element % 2 == 0 {
                 evenArray.append(element)
             }
             else {
-                unevenArray.append(element)
+                oddArray.append(element)
             }
         }
 
         print(evenArray)
-        print(unevenArray)
+        print(oddArray)
         
         // 2) Дан массив опциональных строк. Посчитайте, сколько букв каждая буква встречается во всех словах.
         let optionalStringArray: [String?] = ["aaabbbcde", "nil", "hhhhhh"]
@@ -51,13 +51,13 @@ class ViewController: UIViewController {
         print(myPow(3, nil))
         print(subtract(3, 5))
         print(subtract(3, nil))
-        print(devide(3, 5))
-        print(devide(3, nil))
+        print(divide(3, 5))
+        print(divide(3, nil))
         
         // 4) * Создайте функцию, которая проверяет является ли переданная строка палиндромом. Строка может быть как одним словом (mom) так и предложением (Too bad I hid a boot). В предложении вы должны игнорировать пробелы, большие или маленькие это буквы и знаки препинания при определении полиндрома.
         
-        print(checkPolyndrom("Too bad I hid a boot!"))
-        print(checkPolyndrom("Too, bad, I, hid, a,-?: boot!"))
+        print(checkPalindrom("Too bad I hid a boot!"))
+        print(checkPalindrom("Too, bad, I, hid, a,-?: boot!"))
    }
 
     func add(_ firstNumber: Int?, to secondNumber: Int?) -> String {
@@ -88,7 +88,7 @@ class ViewController: UIViewController {
         String((firstNumber ?? 0) - (secondNumber ?? 0))
     }
     
-    func devide(_ firstNumber: Double?, _ secondNumber: Double?) -> String {
+    func divide(_ firstNumber: Double?, _ secondNumber: Double?) -> String {
         guard let firstNumber = firstNumber,
               let secondNumber = secondNumber else {
             return "Одно из значений не число"
@@ -120,12 +120,12 @@ class ViewController: UIViewController {
         print(result)
     }
     
-    func checkPolyndrom(_ polyndromString: String?) -> String {
-        guard let polyndromString = polyndromString else {
+    func checkPalindrom(_ polindromString: String?) -> String {
+        guard let polindromString = polindromString else {
             return "Введите строку"
         }
         let charactersToRemove = Set(Array(".:?,-!"))
-        let lowerWithoutWhitespacesString = polyndromString.filter{ !$0.isWhitespace }.lowercased().reduce("") { let str = String($1); return $0 + (charactersToRemove.contains($1) ? "" : str) }
+        let lowerWithoutWhitespacesString = polindromString.filter{ !$0.isWhitespace }.lowercased().reduce("") { let str = String($1); return $0 + (charactersToRemove.contains($1) ? "" : str) }
         var reverceString = String()
         
         for element in lowerWithoutWhitespacesString {
